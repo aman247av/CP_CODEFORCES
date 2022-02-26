6b5c5887cf7d0c2515f2f718b1d5d3c3b9327f94
Aman Verma <aman247av@gmail.com>
2022-02-26 23:59:59 +0530

    D4

--- A_I_Wanna_Be_the_Guy.cpp
+++ A_I_Wanna_Be_the_Guy.cpp
@@ -2,11 +2,17 @@
 using namespace std;
 
 int main(){
-    int lvl;
-    cin>>lvl;
+    int lvl,x1,x2;
+    cin>>lvl>>x1;
     set<int> s;
 
-    for(int i=0;i<2*lvl;i++){
+    for(int i=0;i<x1;i++){
+        int n;
+        cin>>n;
+        s.insert(n);
+    }
+    cin>>x2;
+    for(int i=0;i<x2;i++){
         int n;
         cin>>n;
         s.insert(n);
@@ -19,7 +25,7 @@ int main(){
             cout<<"Oh, my keyboard!";
     else  
         if(s.size()==lvl)
-          cout<<"I become the guy.";
+            cout<<"I become the guy.";
         else
             cout<<"Oh, my keyboard!"; 
 
