17f47c2eed00f76d55d7e56b89a2dad1ac6d561f
Aman Verma <aman247av@gmail.com>
2022-02-28 23:22:42 +0530

    D5

---
+++ A_I_love_username.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int len;
+    cin>>len;
+    int arr[len];
+    for(int i=0;i<len;i++){
+        cin>>arr[i];
+    }
+    
+    int min=arr[0];
+int max=arr[0];
+int c=0;
+for(int i=1;i<len;i++){
+    if(arr[i]>max) {
+        c++;
+        max=arr[i];
+    }
+    else if(arr[i]<min)
+    {
+        min=arr[i];
+        c++;
+    }
+    else
+        continue;
+
+}
+cout<<c;
+
+//     int max=0,min=10000;
+//     for(int i=1;i<len;i++){
+//         for(int j=0;j<=i;j++){
+//             if(arr[j]>max || arr[j]<min){
+//                 c++;
+//             }
+//             if(arr[j]>max){
+//                 max=arr[j];
+//             }
+//             if(arr[j]<min){
+//                 min=arr[j];
+//             }
+            
+//         }
+//        // cout<<c<<"\n";
+//     }
+//     if(c!=0)
+// cout<<c-1<<"\n";
+// else
+// cout<<c;
+    return 0;
+}
